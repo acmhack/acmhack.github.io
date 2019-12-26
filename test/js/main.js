b165c997d4de533a7014dc3d08b6d7451713afd7
Dylan <djbn65@umsystem.edu>
2019-12-26 13:00:23 -0600

    First commit for new site

--- test/js/main.js
+++ test/js/main.js
@@ -1,11 +1,26 @@
-$( window ).ready(function() {
-    var wHeight = $(window).height();
-    $('.slide')
-    .scrollie({
-        scrollOffset : 0,
-        scrollingInView : function(elem) {
-            var bgColor = elem.data('background');
-            $('body').css('background', bgColor); 
-        }
+const albumLength = 516.25;
+var currentSlide = 2;
+var currentLeft = parseInt($('.schedule-slider').css('margin-left'));
+
+$(document).ready(function() { 
+    window.addEventListener("resize", function() {
+        $('.schedule-slider').css('margin-left', 'calc(50% + 83.125px + ' + ((2 - currentSlide) * (albumLength + 96.25)) + 'px)');
+        currentLeft = parseInt($('.schedule-slider').css('margin-left'));
     });
 });
+
+function schedulePrev() {
+    if( currentSlide != 1 ) {
+        currentLeft += albumLength + 96.25;
+        $('.schedule-slider').css('margin-left', currentLeft + 'px');
+        currentSlide--;
+    }
+}
+
+function scheduleNext() {
+    if( currentSlide != $('.album').length ) {
+        currentLeft += (-albumLength - 96.25);
+        $('.schedule-slider').css('margin-left', currentLeft + 'px');
+        currentSlide++;
+    }
+}
