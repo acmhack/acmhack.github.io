1026bb1a7e6cf4a590dc8ad65f152dbaa3a64b2f
Dylan <djbn65@umsystem.edu>
2019-12-31 13:24:43 -0600

    FAQ now works

--- test/js/main.js
+++ test/js/main.js
@@ -9,6 +9,22 @@ $(document).ready(function() {
     });
 });
 
+var coll = document.getElementsByClassName("faq-question");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    content.classList.toggle("active");
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 12 + "px";
+    } 
+  });
+}
+
 function schedulePrev() {
     if( currentSlide != 1 ) {
         currentLeft += albumLength + 96.25;
