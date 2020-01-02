149d4c74d7b5bb12b45d528199ea1282b444d696
Dylan <djbn65@umsystem.edu>
2020-01-02 17:24:45 -0600

    Better scroll snapping

--- test/js/main.js
+++ test/js/main.js
@@ -5,43 +5,49 @@ var currentLeft = parseInt($('.schedule-slider').css('margin-left'));
 var coll = document.getElementsByClassName("faq-question");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    content.classList.toggle("active");
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + 12 + "px";
-    } 
-  });
-}
-
-$('a[href*="#"]').on('click', function(e) {
-    e.preventDefault();
-
-    var snaps = $(".snap");
-    snaps.each( function() {
-        $(this).removeClass("snap");
-    });
-
-    $('.scrollable-content').animate({
-        scrollTop: $(".scrollable-content").scrollTop() + $($(this).attr('href')).position().top
-      },
-      500,
-      function() {
-        snaps.each( function() {
-            $(this).addClass("snap");
-        });
-    });
-});
-
 $(document).ready(function() { 
     window.addEventListener("resize", function() {
         $('.schedule-slider').css('margin-left', 'calc(50% + 83.125px + ' + ((2 - currentSlide) * (albumLength + 96.25)) + 'px)');
         currentLeft = parseInt($('.schedule-slider').css('margin-left'));
     });
+
+    $(document).scrollsnap({
+        snaps: '.snap',
+        proximity: 250,
+        easing: 'easeOutBack'
+    });
+
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        // var snaps = $(".snap");
+        // snaps.each( function() {
+        //     $(this).removeClass("snap");
+        // });
+    
+        $('body,html').animate({
+            scrollTop: $($(this).attr('href')).position().top
+          },
+          500,
+          function() {
+            // snaps.each( function() {
+            //     $(this).addClass("snap");
+            // });
+        });
+    });
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            content.classList.toggle("active");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 12 + "px";
+            } 
+        });
+    }
 });
 
 function schedulePrev() {
